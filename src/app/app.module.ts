917dbfb90c80436fa4a438a782ff7fee0aa9b7a3
chanuwat Saleephan <s5811423105@pkru.ac.th>
2018-07-23 16:06:42 +0700

    updet wep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotonComponent } from './jumboton/jumboton.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    NavbarComponent,
+    JumbotonComponent,
+    FooterComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
